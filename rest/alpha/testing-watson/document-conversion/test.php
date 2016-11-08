12c3d16b65dfbd791a11993e8d0a634a75effa9a
Keith Weaver <keithmweaver@gmail.com>
2016-11-08 09:01:49 -0500

    New Feature. Document conversion

---
+++ rest/alpha/testing-watson/document-conversion/test.php
@@ -0,0 +1,10 @@
+<form enctype="multipart/form-data" method="POST" action="./convert.php">
+	<label>Convert PDF To:</label>
+	<select name="type">
+		<option>answer_units</option>
+		<option>normalized_html</option>
+		<option>normalized_text</option>
+	</select><br/>
+	<input type="file" name="bookFile"/><br/>
+	<button type="submit">Upload</button>
+</form>
