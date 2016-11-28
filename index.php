0bc7dfc124d3a5459edce07e426080be1c44e4fb
Keith Weaver <keithmweaver@gmail.com>
2016-11-28 10:15:28 -0500

    Added Basic Styling for website

---
+++ index.php
@@ -0,0 +1,92 @@
+<html>
+<head>
+<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:700" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Fjalla+One|Lato" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<link rel="stylesheet" href="./css/main/top.css">
+<link rel="stylesheet" href="./css/main/disclaimer.css">
+<link rel="stylesheet" href="./css/main/search.css">
+<link rel="stylesheet" href="./css/main/search-content.css">
+
+</head>
+<body>
+
+<div class="container max-width-wrapper">
+	<div class="row title-row">
+		<div class="col-sm-12 text-center">
+			<h1 class="title">peggy</h1>
+		</div>
+	</div>
+</div>
+
+<span id="page-content">
+	<!--
+	<div class="container max-search-wrapper">
+		<div class="row search-nothing-row">
+			<div class="col-sm-12">
+			<p class="search-error-label">Error: And more and more and more and mroe and rmoe</p>
+			<input type="text" class="search-textbox">
+			<p class="search-help-text">Press enter to search</p>
+
+			</div>
+		</div>
+	</div>
+	-->
+	
+	<div class="container max-search-wrapper">
+		<div class="row">
+			<div class="col-sm-12">
+				<p class="search-error-label">Error: And more and more and more and mroe and rmoe</p>
+				<input type="text" class="search-textbox">
+				<p class="search-help-text">Press enter to search</p>
+			</div>
+		</div>
+	</div>
+	<div class="search-wrapper">
+
+		<div class="container max-width-wrapper">
+
+			<div class="row search-results-row">
+				<div class="col-sm-12">
+					<h2 class="search-results-title">Best Answer:</h2>
+					<p class="search-results-answer">
+						And mroe text would go here nad such and more and more and more and rmoe and mroe and mroe and mroe and test ths ij dadsas0 ji onan in in ina ioni nion ionion in oin oin oin ion ionioa nsdiooi aisnd iaon dioan ion ioaiodaio dnioan inaidn aisndiandio nai ni ionoi nio ino n
+					</p>
+					<p class="search-results-confidence-text">0.39</p>
+
+				</div>
+			</div>
+
+
+			<div class="row search-results-row">
+				<div class="col-sm-12">
+					<h2 class="search-results-title">Other Answer:</h2>
+					<p class="search-results-answer">
+						And mroe text would go here nad such and more and more and more and rmoe and mroe and mroe and mroe and test ths ij dadsas0 ji onan in in ina ioni nion ionion in oin oin oin ion ionioa nsdiooi aisnd iaon dioan ion ioaiodaio dnioan inaidn aisndiandio nai ni ionoi nio ino n
+					</p>
+					<p class="search-results-confidence-text">0.39</p>
+
+				</div>
+			</div>
+
+
+		</div>
+	</div>
+
+</span>
+
+<!-- 
+<div class="disclaimer-wrapper">
+	<p class="disclaimer-text">
+		Disclaimer: Please consult a medical professional for better advice.
+	</p>
+</div>
+-->
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<!-- <script src="./js/main/load.js"></script> -->
+</body>
+</html>
